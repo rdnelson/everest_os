fb9f584392e5a7e39ebb40683c3ff472deca84c6
Robert Nelson <robert@rnelson.ca>
2015-05-05 11:33:21 -0400

    Cleaned up interrupt handlers a bit

--- kernel/interrupts.s
+++ kernel/interrupts.s
@@ -1,97 +1,67 @@
-[extern isr_handler]
-
-%macro ISR_NOERRCODE 1
+%macro ISR_NOERRCODE 2
+  %ifnidn %2,isr_nothing
+    [EXTERN %2]
+  %endif
   [GLOBAL isr%1]
   isr%1:
-    cli
-    push 0
-    push byte %1
-    jmp isr_common_stub
-%endmacro
-
-%macro ISR_ERRCODE 1
-  [GLOBAL isr%1]
-  isr%1:
-    cli
-    push byte %1
-    jmp isr_common_stub
-%endmacro
-
-%macro ISR_DF 1
-  [GLOBAL isr%1]
-  isr%1:
-    cli
-    mov eax, [esp]
-    mov ebx, [esp + 4]
-    mov ecx, [esp + 8]
+    pushad
+    cld
+    call %2
+    popad
     iret
 %endmacro
 
-%macro ISR_GPF 1
+%macro ISR_ERRCODE 2
+  %ifnidn %2,isr_nothing
+    [EXTERN %2]
+  %endif
   [GLOBAL isr%1]
   isr%1:
-    cli
-    mov eax, [esp]
-    mov ebx, [esp + 4]
-    mov ecx, [esp + 8]
-    jmp $
+    pushad
+    mov eax, [esp + 32]
+    cld
+    push eax
+    call %2
+    add esp, 4
+    popad
+    add esp, 4
+    iret
 %endmacro
 
-isr_common_stub:
-;  pusha
-;
-;  mov ax, ds
-;  push eax
-;
-;  mov ax, 0x10
-;  mov ds, ax
-;  mov es, ax
-;  mov fs, ax
-;  mov gs, ax
-;
-;  ;call isr_handler
-;
-;  pop eax
-;  mov ds, ax
-;  mov es, ax
-;  mov fs, ax
-;  mov gs, ax
-;
-;  popa
-  add esp, 8
-  sti
-  iret
+isr_nothing:
+  ret
 
-ISR_NOERRCODE 0
-ISR_NOERRCODE 1
-ISR_NOERRCODE 2
-ISR_NOERRCODE 3
-ISR_NOERRCODE 4
-ISR_NOERRCODE 5
-ISR_NOERRCODE 6
-ISR_NOERRCODE 7
-ISR_NOERRCODE 8
-ISR_NOERRCODE 9
-ISR_ERRCODE 10
-ISR_ERRCODE 11
-ISR_ERRCODE 12
-ISR_ERRCODE 13
-ISR_ERRCODE 14
-ISR_NOERRCODE 15
-ISR_NOERRCODE 16
-ISR_ERRCODE 17
-ISR_NOERRCODE 18
-ISR_NOERRCODE 19
-ISR_NOERRCODE 20
-ISR_NOERRCODE 21
-ISR_NOERRCODE 22
-ISR_NOERRCODE 23
-ISR_NOERRCODE 24
-ISR_NOERRCODE 25
-ISR_NOERRCODE 26
-ISR_NOERRCODE 27
-ISR_NOERRCODE 28
-ISR_NOERRCODE 29
-ISR_NOERRCODE 30
-ISR_NOERRCODE 31
-ISR_NOERRCODE 32
+ISR_NOERRCODE 0,  isr_nothing
+ISR_NOERRCODE 1,  isr_nothing
+ISR_NOERRCODE 2,  isr_nothing
+ISR_NOERRCODE 3,  isr_nothing
+ISR_NOERRCODE 4,  isr_nothing
+ISR_NOERRCODE 5,  isr_nothing
+ISR_NOERRCODE 6,  isr_nothing
+ISR_NOERRCODE 7,  isr_nothing
+ISR_ERRCODE   8,  isr_nothing
+ISR_NOERRCODE 9,  isr_nothing
+ISR_ERRCODE   10, isr_nothing
+ISR_ERRCODE   11, isr_nothing
+ISR_ERRCODE   12, isr_nothing
+ISR_ERRCODE   13, isr_nothing
+ISR_ERRCODE   14, isr_nothing
+ISR_NOERRCODE 15, isr_nothing
+ISR_NOERRCODE 16, isr_nothing
+ISR_ERRCODE   17, isr_nothing
+ISR_NOERRCODE 18, isr_nothing
+ISR_NOERRCODE 19, isr_nothing
+ISR_NOERRCODE 20, isr_nothing
+ISR_NOERRCODE 21, isr_nothing
+ISR_NOERRCODE 22, isr_nothing
+ISR_NOERRCODE 23, isr_nothing
+ISR_NOERRCODE 24, isr_nothing
+ISR_NOERRCODE 25, isr_nothing
+ISR_NOERRCODE 26, isr_nothing
+ISR_NOERRCODE 27, isr_nothing
+ISR_NOERRCODE 28, isr_nothing
+ISR_NOERRCODE 29, isr_nothing
+ISR_NOERRCODE 30, isr_nothing
+ISR_NOERRCODE 31, isr_nothing
+ISR_NOERRCODE 32, isr_nothing
+
